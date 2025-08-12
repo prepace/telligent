9f857b21946e4e439dbfd58dc325e40e35879116
prepace <pace@flyingovertime.org>
2025-08-12 15:56:59 -0500

    active date

--- src/components/Masthead.tsx
+++ src/components/Masthead.tsx
@@ -1,12 +1,19 @@
 
 export default function Masthead() {
   return (
-    <div className="border-b border-gray-200 py-4">
-      <div className="container mx-auto px-4 flex flex-col items-center">
-        <div className="text-xs  mb-2">March 12, 2025</div>
-        <div className="font-serif text-4xl md:text-6xl font-bold tracking-tight text-center">Telligent</div>
-        <div className="text-sm  mt-2">Mental Wellness for a Better Tomorrow</div>
-      </div>
-    </div>
-  )
+			<div className="border-b border-gray-200 py-4">
+				<div className="container mx-auto px-4 flex flex-col items-center">
+					<div className="text-xs  mb-2">
+						{new Date().toLocaleString("default", { month: "long" })}{" "}
+						{new Date().getDate()}, {new Date().getFullYear()}
+					</div>
+					<div className="font-serif text-4xl md:text-6xl font-bold tracking-tight text-center">
+						Telligent
+					</div>
+					<div className="text-sm  mt-2">
+						Mental Wellness for a Better Tomorrow
+					</div>
+				</div>
+			</div>
+		);
 }
